3092781ad4f23f5785fd746efd3256fa0d9d27ff
Dmytro <dmytro.kutsenko1996@gmail.com>
2025-08-28 15:00:57 +0300

    allow_write_operations_v2

--- api/server.js
+++ api/server.js
@@ -12,12 +12,11 @@ const db = JSON.parse(data);
 const router = jsonServer.router(db);
 
 // Comment out to allow write operations
-// const router = jsonServer.router("db.json");
+// const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-
 // Add this before server.use(router)
 server.use(
   jsonServer.rewriter({
@@ -32,37 +31,3 @@ server.listen(3000, () => {
 
 // Export the Server API
 module.exports = server;
-
-/*
-// api/server.js
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // твой JSON файл
-const middlewares = jsonServer.defaults();
-
-// стандартные middlewares
-server.use(middlewares);
-
-// CORS middleware
-server.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  // preflight-запросы
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  next();
-});
-
-// подключаем json-server router
-server.use(router);
-
-// экспортируем для Vercel
-module.exports = server;
-*/
